3755d75f40820570817b4b4dc1bee68227a1f60f
Anish Varshney <anishvarshney912@gmail.com>
2024-09-01 16:06:12 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import Home from "./pages/Home";
+import ProductList from './pages/ProductList';
+import Product from './pages/Product';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate 
+} from "react-router-dom";
+import Success from './pages/Success';
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const user = useSelector((state) => state.user.currentUser);
   return (
-   <Home/>
+   <Router>
+    <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/products/:categories" element = {<ProductList/>}/>
+      <Route path = "/product/:id" element = {<Product/>}/>
+      <Route path = "/cart" element = {<Cart/>}/>
+      <Route path = "/success" element = {<Success/>}/>
+
+      <Route path = "/login" element = {
+        user ? <Navigate to = "/" replace/> : <Login/> }/>
+
+      <Route path = "/register" element = {
+        user ? <Navigate to  = "/" replace/> : <Register/> }/>
+
+    </Routes>
+   </Router>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from '@mui/icons-material';
 import { Badge } from "@mui/material";
 import { mobile } from "../responsive";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   height: 60px;
@@ -67,6 +69,9 @@ const MenuItem = styled.div`
 `;
 
 const Navbar = () => {
+  const quantity = useSelector(state=>state.cart.quantity)
+  // console.log(cart);
+  
   return (
     <Container>
       <Wrapper>
@@ -83,11 +88,13 @@ const Navbar = () => {
         <Right>
           <MenuItem>REGISTER</MenuItem>
           <MenuItem>SIGN IN</MenuItem>
+          <Link to="/cart">
           <MenuItem>
-            <Badge badgeContent={4} color="primary">
+            <Badge badgeContent={quantity} color="primary">
               <ShoppingCartOutlined />
             </Badge>
           </MenuItem>
+          </Link>
         </Right>
       </Wrapper>
     </Container>
