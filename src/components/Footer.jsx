6b14fc4fb2b9bf4149b5a2a33e1076c8d48fba6e
Anish Varshney <anishvarshney912@gmail.com>
2024-09-22 19:08:40 +0530

    small changes for Hosting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,38 +1,39 @@
 import {
-    Facebook,
-    Instagram,
-    MailOutline,
-    Phone,
-    Pinterest,
-    Room,
-    Twitter,
-  } from "@mui/icons-material";
-  import styled from "styled-components";
-  import { mobile } from "../responsive";
-  
-  const Container = styled.div`
+  Facebook,
+  Instagram,
+  MailOutline,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
     display: flex;
     ${mobile({ flexDirection: "column" })}
   `;
-  
-  const Left = styled.div`
+
+const Left = styled.div`
     flex: 1;
     display: flex;
     flex-direction: column;
     padding: 20px;
   `;
-  
-  const Logo = styled.h1``;
-  
-  const Desc = styled.p`
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
     margin: 20px 0px;
   `;
-  
-  const SocialContainer = styled.div`
+
+const SocialContainer = styled.div`
     display: flex;
   `;
-  
-  const SocialIcon = styled.div`
+
+const SocialIcon = styled.div`
     width: 40px;
     height: 40px;
     border-radius: 50%;
@@ -43,101 +44,118 @@ import {
     justify-content: center;
     margin-right: 20px;
   `;
-  
-  const Center = styled.div`
+
+const Center = styled.div`
     flex: 1;
     padding: 20px;
     ${mobile({ display: "none" })}
   `;
-  
-  const Title = styled.h3`
+
+const Title = styled.h3`
     margin-bottom: 30px;
   `;
-  
-  const List = styled.ul`
+
+const List = styled.ul`
     margin: 0;
     padding: 0;
     list-style: none;
     display: flex;
     flex-wrap: wrap;
   `;
-  
-  const ListItem = styled.li`
+
+const ListItem = styled.li`
     width: 50%;
     margin-bottom: 10px;
   `;
-  
-  const Right = styled.div`
+
+const Right = styled.div`
     flex: 1;
     padding: 20px;  
     ${mobile({ backgroundColor: "#fff8f8" })}
   `;
-  
-  const ContactItem = styled.div`
+
+const ContactItem = styled.div`
     margin-bottom: 20px;
     display: flex;
     align-items: center;
   `;
-  
-  const Payment = styled.img`
+
+const Payment = styled.img`
       width: 50%;
   `;
-  
-  const Footer = () => {
-    return (
-      <Container>
-        <Left>
-          <Logo>SHYAM.</Logo>
-          <Desc>
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form, by injected
-            humour, or randomised words which don’t look even slightly believable.
-          </Desc>
-          <SocialContainer>
-            <SocialIcon color="3B5999">
-              <Facebook />
-            </SocialIcon>
-            <SocialIcon color="E4405F">
-              <Instagram />
-            </SocialIcon>
-            <SocialIcon color="55ACEE">
-              <Twitter />
-            </SocialIcon>
-            <SocialIcon color="E60023">
-              <Pinterest />
-            </SocialIcon>
-          </SocialContainer>
-        </Left>
-        <Center>
-          <Title>Useful Links</Title>
-          <List>
-            <ListItem>Home</ListItem>
-            <ListItem>Cart</ListItem>
-            <ListItem>Man Fashion</ListItem>
-            <ListItem>Woman Fashion</ListItem>
-            <ListItem>Accessories</ListItem>
-            <ListItem>My Account</ListItem>
-            <ListItem>Order Tracking</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Wishlist</ListItem>
-            <ListItem>Terms</ListItem>
-          </List>
-        </Center>
-        <Right>
-          <Title>Contact</Title>
-          <ContactItem>
-            <Room style={{marginRight:"10px"}}/> Shyam Agency, College Road Atrauli Aligarh
-          </ContactItem>
-          <ContactItem>
-            <Phone style={{marginRight:"10px"}}/> +91 95486 93900
-          </ContactItem>
-          <ContactItem>
-            <MailOutline style={{marginRight:"10px"}} /> shyamagency@gmail.com
-          </ContactItem>
-          <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
-        </Right>
-      </Container>
-    );
-  };
-  
-  export default Footer;
+
+const StyledLink = styled(Link)`   
+  text-decoration: none;  
+  color: inherit;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Voguevault.</Logo>
+        <Desc>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don’t look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+
+
+          <ListItem>
+            <StyledLink to="/">
+              Home
+            </StyledLink>
+          </ListItem>
+
+          <ListItem>
+            <StyledLink to="/cart">
+              Cart
+            </StyledLink>
+          </ListItem>
+
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> Shyam Agency, College Road Atrauli Aligarh
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +91 95486 93900
+        </ContactItem>
+        <ContactItem>
+          <MailOutline style={{ marginRight: "10px" }} /> shyamagency@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -88,7 +88,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>SHYAM.</Logo>
+          <Logo>Voguevault.</Logo>
         </Center>
         <Right>
 
