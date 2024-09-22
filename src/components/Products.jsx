163c71d32efa1bdb55bd64a2b7036f9bcca81619
Anish Varshney <anishvarshney912@gmail.com>
2024-09-22 14:18:53 +0530

    small changes for hosting

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:5000/api/products?category=${cat}`
-            : "http://localhost:5000/api/products"
+            ? `https://voguevault.onrender.com/api/products?category=${cat}`
+            : "https://voguevault.onrender.com/api/products"
         );
         // console.log(res);
         setProducts(res.data);
